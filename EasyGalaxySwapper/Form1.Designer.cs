937f18d5845756e0f5d60f508559e32264926f0f
雷さん <admin@uplauncher.xyz>
2023-08-20 19:57:21 +0900

    プロジェクト ファイルを追加します。

---
+++ EasyGalaxySwapper/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace EasyGalaxySwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AllProgress = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MenuProgress = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(476, 215);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(49, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EasyGalaxy Swapper";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 10F);
+            this.label3.Location = new System.Drawing.Point(19, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(309, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "β教祖が作成したEasyGalaxy SwapperのGUI版です。\r\n製作者には敬意を持ちましょう。";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AllProgress
+            // 
+            this.AllProgress.Location = new System.Drawing.Point(23, 192);
+            this.AllProgress.Name = "AllProgress";
+            this.AllProgress.Size = new System.Drawing.Size(291, 22);
+            this.AllProgress.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label5.Location = new System.Drawing.Point(19, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ダウンロードを開始してください";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(24, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ダウンロードを開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MenuProgress
+            // 
+            this.MenuProgress.Location = new System.Drawing.Point(23, 220);
+            this.MenuProgress.Name = "MenuProgress";
+            this.MenuProgress.Size = new System.Drawing.Size(291, 22);
+            this.MenuProgress.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(201, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 33);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "公式Discord";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(20)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(339, 302);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.MenuProgress);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AllProgress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EasyGalaxy Swapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar AllProgress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar MenuProgress;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EasyGalaxySwapper/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace EasyGalaxySwapper
+{
+    public partial class Form1 : Form
+    {
+        static bool NetRuntimeInstalled = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button1.Text = "処理中...";
+            Process netChecker = new Process();
+            netChecker.StartInfo.FileName = "dotnet.exe";
+            netChecker.StartInfo.Arguments = "--list-runtimes";
+            netChecker.StartInfo.UseShellExecute = false;
+            netChecker.StartInfo.RedirectStandardOutput = true;
+            netChecker.Start();
+            string results = netChecker.StandardOutput.ReadToEnd();
+            netChecker.WaitForExit();
+            netChecker.Close();
+
+            if (results.Contains("Microsoft.WindowsDesktop.App 7.0."))
+            {
+                AllProgress.Value = 50;
+                DownloadGalaxySwapper();
+            } else
+            {
+                InstallNetRuntime();
+            }
+        }
+
+        private void InstallNetRuntime()
+        {
+            label5.Text = ".NETをダウンロード中";
+            WebClient wc = new WebClient();
+            wc.DownloadFileAsync(new Uri("https://download.visualstudio.microsoft.com/download/pr/5b2fbe00-507e-450e-8b52-43ab052aadf2/79d54c3a19ce3fce314f2367cf4e3b21/windowsdesktop-runtime-7.0.0-win-x64.exe"), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp", "windowsdesktop-runtime-7.0.0-win-x64.exe"));
+            wc.DownloadProgressChanged += NetRuntime_DownloadProgressChanged;
+            wc.DownloadFileCompleted += NetRuntime_DownloadFileCompleted;
+        }
+
+        private void NetRuntime_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            AllProgress.Value = 25;
+            label5.Text = ".NETをインストール中";
+            Process netRuntime = new Process();
+            netRuntime.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp", "windowsdesktop-runtime-7.0.0-win-x64.exe");
+            netRuntime.StartInfo.Arguments = "/quiet";
+            netRuntime.StartInfo.UseShellExecute = true;
+            netRuntime.EnableRaisingEvents = true;
+            netRuntime.SynchronizingObject = this;
+            netRuntime.Start();
+            netRuntime.Exited += NetRuntime_Exited;
+        }
+
+        private void NetRuntime_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            MenuProgress.Value = e.ProgressPercentage;
+        }
+
+        private void NetRuntime_Exited(object sender, EventArgs e)
+        {
+            AllProgress.Value = 50;
+            DownloadGalaxySwapper();
+        }
+
+        private void DownloadGalaxySwapper()
+        {
+            label5.Text = "Galaxy Swapperをダウンロード中";
+            WebClient wc = new WebClient();
+            wc.DownloadFileAsync(new Uri("https://galaxyswapperv2.com/Downloads/Galaxy%20Swapper%20v2.exe"), Path.Combine(Directory.GetCurrentDirectory(), "GalaxySwapperV2.exe"));
+            wc.DownloadProgressChanged += Galaxy_DownloadProgressChanged;
+            wc.DownloadFileCompleted += Galaxy_DownloadFileCompleted;
+        }
+
+        private async void Galaxy_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            AllProgress.Value = 75;
+            Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "GalaxySwapperV2.exe"));
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://galaxyswapperv2.com/Key/Create.php");
+            var LicenseKeyParser = HttpUtility.ParseQueryString(response.RequestMessage.RequestUri.Query);
+            var LicenseKey = LicenseKeyParser["key"];
+            Clipboard.SetText(LicenseKey);
+            label5.Text = "ライセンスキー: " + LicenseKey + Environment.NewLine + "(クリップボードにコピーしました)";
+            AllProgress.Value = 100;
+            button1.Text = "再実行";
+            button1.Enabled = true;
+
+        }
+
+        private void Galaxy_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            MenuProgress.Value = e.ProgressPercentage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://galaxyswapperv2.com/Discord.php");
+        }
+    }
+}
